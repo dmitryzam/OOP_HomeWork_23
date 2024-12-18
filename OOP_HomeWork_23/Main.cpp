876af5858fbcef2a2fea3498e934f821c12bbc22
dmitryzam <dmitryzam@mail.ru>
2024-12-18 20:11:45 +0500

    Add project files.

---
+++ OOP_HomeWork_23/Main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include"Time.hpp"
+#include<exception>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	try {
+		std::cout << "Создадим объект t1 класса Time по умолчанию:\n";
+		Time t1;
+		std::cout << "t1 - " << t1;
+		std::cout << "Используем функцию t1.calculateTime(10, 130, 100):\n";
+		t1.calculateTime(10, 130, 100);
+		std::cout << "t1 - " << t1;
+
+		std::cout << "\nСоздадим объект t2(1060) c заданным значением секунд:\n";
+		Time t2(1060);
+		std::cout << "t2 - " << t2;
+		std::cout << "\nСоздадим объект t3(1, 10, 15) c заданными значениями (ЧЧ:ММ:СС):\n";
+		Time t3(1,10,15);
+		std::cout << "t3 - " << t3;
+		std::cout << "\nСоздадим объект t4(30, 10, 15) c заданными значениями и поймаем исключение:\n";
+		Time t4(30, 10, 15);
+		std::cout << t4;
+	}
+	catch (std::invalid_argument& log) {
+		std::cerr << "Error: " << log.what() << std::endl;
+	}
+
+	return 0;
+}
---
+++ OOP_HomeWork_23/Time.hpp
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include<iostream>
+#include<exception>
+#include<iomanip>
+// подкласс Часы:
+class Watch {
+public:
+	Watch() : m_hour(0), m_min(0), m_sec(0){}
+	Watch(unsigned sec) {
+		m_min = sec / 60;
+		m_sec = sec % 60;
+		m_hour = m_min / 60;
+		m_min = m_min % 60;
+	}	
+	friend std::ostream& operator<<(std::ostream& out, const Watch& obj) {
+		out << std::setw(2) << std::setfill('0') << obj.m_hour << ':' << std::setw(2) <<
+			obj.m_min << ':' << std::setw(2) << obj.m_sec << std::endl;
+		return out;
+	}
+private:
+	unsigned m_hour;
+	unsigned m_min;
+	unsigned m_sec;
+};
+// основной класс:
+class Time {
+private:
+	unsigned my_sec;
+	Watch m_time;
+public:
+	Time() : my_sec(0){
+		m_time = Watch();
+	}
+	Time(unsigned sec) : my_sec(sec){		
+		m_time = Watch(my_sec);
+	}
+	Time(const unsigned hrs, const unsigned min, const unsigned sec){
+		// "броски" при неправильном вводе значений:
+		if (hrs > 24)
+			throw std::invalid_argument("The value \'hours\' is entered incorrectly.");
+		if(min > 60)
+			throw std::invalid_argument("The value \'minutes\' is entered incorrectly.");
+		if(sec > 60)
+			throw std::invalid_argument("The value \'seconds\' is entered incorrectly.");
+		my_sec = hrs * 3600 + min * 60 + sec;
+		m_time = Watch(my_sec);
+	}
+	void calculateTime(unsigned hrs, unsigned min, unsigned sec) {
+		my_sec = hrs * 3600 + min * 60 + sec;
+		m_time = Watch(my_sec);
+	}
+	friend std::ostream& operator<<(std::ostream& out, const Time& obj) {
+		out << obj.m_time;
+		return out;
+	}
+};
